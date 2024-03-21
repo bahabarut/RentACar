6be9534f072ec705a4797c8b4d72bce2e7eab304
Baha Barut <20201129503@ogr.akdeniz.edu.tr>
2024-03-21 15:49:40 +0300

    Start to CQRS MediatR create brand command handler business rule

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -0,0 +1,49 @@
+﻿using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.CreateBrand
+{
+    public class CreateBrandCommand : IRequest<CreatedBrandDto> //out type then create process
+    {
+        //data properties for save db
+        public string Name { get; set; }
+
+        public class CreateBrandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandDto>
+        {
+            private readonly IBrandRepository _brandRepository;
+            private readonly IMapper _mapper;
+            private readonly BrandBusinessRules _brandBusinessRules;
+
+            public CreateBrandHandler(IBrandRepository brandRepository, IMapper mapper, BrandBusinessRules brandBusinessRules)
+            {
+                _brandRepository = brandRepository;
+                _mapper = mapper;
+                _brandBusinessRules = brandBusinessRules;
+            }
+
+            public async Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                // business rules or other control and add to db area
+                await _brandBusinessRules.BrandNameCanNotBeDuplicatedWhenInserted(request.Name);
+
+                //map request to entity type for db
+                Brand mappedBrand = _mapper.Map<Brand>(request);
+                // add entity to db
+                Brand createdBrand = await _brandRepository.AddAsync(mappedBrand);
+                // map entity to dto for users (entity self is not send directly to the user)
+                CreatedBrandDto createdBrandDto = _mapper.Map<CreatedBrandDto>(createdBrand);
+                return createdBrandDto;
+            }
+        }
+    }
+}
--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -1,9 +1,11 @@
 using Persistence;
+using Application;
+using Core.CrossCuttingConcerns.Exceptions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-//builder.Services.AddApplicationServices();
+builder.Services.AddApplicationServices();
 //builder.Services.AddSecurityServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
 //builder.Services.AddInfrastructureServices();
@@ -21,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+if (app.Environment.IsProduction())
+    app.ConfigureCustomExceptionMiddleware();
 
 app.UseAuthorization();
 
